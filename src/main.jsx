625c34221c7e2c354e1adceb858cf7c0c770d834
José <jg345762@gmail.com>
2023-04-20 18:49:03 -0300

    added HashRouter provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context/AppProvider";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
